d049cfb0b045e386e2603a20a22be5d7df90c8b4
Adrián <adrianramaaguilar@gmail.com>
2021-01-11 15:33:11 -0400

    Gilden rose kata firsts reqs done with 95 in rubicritic

---
+++ code/tests/gilden_rose_spec.rb
@@ -0,0 +1,12 @@
+require_relative "../libs/gilden_rose"
+require_relative "../libs/item"
+
+describe GildedRose do
+  describe "#update_quality" do
+    it "does not change the name" do
+      items = [Item.new("foo", 0, 0)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].name).to eq "foo"
+    end
+  end
+end
