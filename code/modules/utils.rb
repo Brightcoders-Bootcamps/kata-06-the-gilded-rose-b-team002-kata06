734b99dfaa7198706c70b96943025ee569e357c0
Mario Mancilla <mariormt17@gmail.com>
2021-01-14 13:53:00 -0600

    Gilden rose tests finished

--- code/modules/utils.rb
+++ code/modules/utils.rb
@@ -8,22 +8,23 @@ module Utils
       item.quality += 1 if item.quality < 50 && item_name === "Aged Brie"
       item.quality += set_backstage_passes_quality(item.quality, item.sell_in) if (item_name === "Backstage passes to a TAFKAL80ETC concert")
     elsif item_name != "Sulfuras, Hand of Ragnaros"
-      if item.quality < 0
+      if item.quality > 0
         item.quality -= 1 if (item_name != "Conjured item")
         item.quality -= 2 if (item_name === "Conjured item")
       end
     end
   end
   
+  private
   def Utils.set_backstage_passes_quality(item_quality, item_sell_in)
     if (item_quality < 50)
       case item_sell_in
-      when ((item_sell_in <= 10 && item_sell_in >= 6))
-        return (item_quality + 2)
-      when ((item_sell_in <= 5 && item_sell_in >= 1))
-        return (item_quality + 3)
-      when ((item_sell_in > 10))
-        return (item_quality + 1)
+      when 1..5
+        return 3
+      when 6..10
+        return 2
+      else 
+        return 1
       end
     end
     return 0
